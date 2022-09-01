3c8a17e3800aa493f068bfee283f33271c62c8db
Christian Meza <c.meza@bmvglobal.net>
2022-09-01 18:01:41 -0500

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from 'react';
+const url = "https://reqres.in/api/users"
 function App() {
+
+  const [dataApi, setDataApi] = useState({
+    loading: true,
+    data: []
+  })
+  
+  const { data, loading } = dataApi
+  const [ordering, setOrdering] = useState(data?.data)
+
+  const getApi = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+  }
+
+  
+  useEffect(() => {
+    getApi()
+    .then(item => {
+      setDataApi({
+        loading: false,
+        // data: item
+      })
+      setOrdering(item.data)
+      })
+    }, [ ordering ])
+
+  const handleAscending = () => {
+    setOrdering(
+      ordering.sort((a, b) => {
+        if (a.last_name < b.last_name) {
+          return -1;
+        }
+        if (a.last_name > b.last_name) {
+          return 1;
+        }
+
+        return 0
+
+      })
+      )
+      console.log('ascending')
+      console.log(ordering)
+  }
+  const handleDescending = () => {
+    setOrdering(
+      ordering.sort((a, b) => {
+        if (a.last_name > b.last_name) {
+          return -1;
+        }
+        if (a.last_name < b.last_name) {
+          return 1;
+        }
+
+        return 0
+      })
+      )
+      console.log('descending')
+      console.log(ordering)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
+      <div>
+        <button onClick={ handleAscending } >Ascending</button>
+        <button onClick={ handleDescending } >Descending</button>
+      </div>
+      <ul>
+        {
+          !loading && ordering.map(({ id, email, first_name, last_name, avatar }) => (
+            <li key={ id }>
+              <figure>
+                <img src={ avatar } alt=""/>
+              </figure>
+              { first_name } /
+              { last_name } /
+              { email }
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
